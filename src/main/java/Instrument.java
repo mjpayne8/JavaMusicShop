e843313e76838472f378eb5a738ef5d2a4cbf672
Mark Payne <mjpayne8@gmail.com>
2019-11-29 11:23:06 +0000

    Abstract Instrument class implemented

---
+++ src/main/java/Instrument.java
@@ -0,0 +1,13 @@
+public abstract class Instrument {
+
+    private String construction;
+    private String colour;
+    private String type;
+
+    public Instrument(String construction, String colour, String type){
+        this.construction = construction;
+        this.colour = colour;
+        this.type = type;
+    }
+
+}
